1e37e33690ec9f8c68ff782ed70fa7ceb2929a42
DM <276061317@qq.com>
2021-04-21 19:45:39 +0800

    1

--- music-imooc/miniprogram/components/playlist/playlist.js
+++ music-imooc/miniprogram/components/playlist/playlist.js
@@ -1,23 +1,21 @@
 // components/playlist/playlist.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		playlist: {
+			type: Object,
+		},
+	},
 
-  },
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {},
 })
