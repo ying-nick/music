33ba200c3cc73fc82b52020e3aba72dff60ee6b0
DM <276061317@qq.com>
2021-04-21 20:58:01 +0800

    2

--- music-imooc/miniprogram/components/playlist/playlist.js
+++ music-imooc/miniprogram/components/playlist/playlist.js
@@ -8,14 +8,42 @@ Component({
 			type: Object,
 		},
 	},
-
+	observices: {
+		['playlist.playCount'](val) {
+			console.log(val)
+			this.setData({
+				count: this.tranNumber(val, 2),
+			})
+		},
+	},
 	/**
 	 * 组件的初始数据
 	 */
-	data: {},
+	data: {
+		count: 0,
+	},
 
 	/**
 	 * 组件的方法列表
 	 */
-	methods: {},
+	methods: {
+		tranNumber(num, point) {
+			let numStr = num.toString().split('.')[0]
+			if (numStr.length < 6) {
+				return numStr
+			} else if (numStr.length >= 6 && numStr.length <= 8) {
+				let dnum = numStr.substring(
+					numStr.length - 4,
+					numStr.length - 4 + point
+				)
+				return parseFloat(parseInt(num / 10000) + '.' + dnum) + '万'
+			} else if (numStr.length >= 8) {
+				let dnum = numStr.substring(
+					numStr.length - 8,
+					numStr.length - 8 + point
+				)
+				return parseFloat(parseInt(num / 100000000) + '.' + dnum) + '亿'
+			}
+		},
+	},
 })
