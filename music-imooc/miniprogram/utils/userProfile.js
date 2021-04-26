be7e9408e471d17980c29ba39d8017bf02a415da
DM <276061317@qq.com>
2021-04-26 09:07:41 +0800

    1

---
+++ music-imooc/miniprogram/utils/userProfile.js
@@ -0,0 +1,15 @@
+function getUserProfile() {
+	wx.getUserProfile({
+		desc: '用于完善个人资料',
+		success: function (res) {
+			var userInfo = res.userInfo
+			// console.log('userInfo==>', userInfo)
+			wx.setStorageSync('storage_info', 'ok') //本地标记
+			//下面将userInfo存入服务器中的用户个人资料
+			//...
+		},
+		fail() {
+			console.log('用户拒绝授权')
+		},
+	})
+}
