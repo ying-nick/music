4a8261c5629d0e2f178d313ab241de5cad771594
DM <276061317@qq.com>
2021-04-27 16:45:15 +0800

    33

--- music-imooc/miniprogram/components/blog-card/blog-card.js
+++ music-imooc/miniprogram/components/blog-card/blog-card.js
@@ -1,23 +1,28 @@
 // components/blog-card/blog-card.js
+import formatTime from '../../utils/formatTime'
+
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		blog: Object,
+	},
+	//?监听发布时间
+	observers: {
+		['blog.createTime'](val) {
+			if (val) {
+				formatTime(new Date(val))
+			}
+		},
+	},
+	/**
+	 * 组件的初始数据
+	 */
+	data: {},
 
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {},
 })
---
+++ music-imooc/miniprogram/utils/formatTime.js
@@ -0,0 +1,10 @@
+module.exports = (data) => {
+	let fmt = 'yyyy-MM-dd hh:mm:ss'
+	const o = {
+		'M+': date.getMonth() + 1,
+		'd+': date.getDate(),
+		'h+': date.getHours(),
+		'm+': date.getMinutes(),
+		's+': date.getSeconds(),
+	}
+}
