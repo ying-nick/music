175616014ad8b84b9864961a03489fd8895c9dc1
DM <276061317@qq.com>
2021-04-28 16:24:06 +0800

    2

---
+++ music-imooc/miniprogram/components/blog-ctrl/blog-ctrl.js
@@ -0,0 +1,23 @@
+// components/blog-ctrl/blog-ctrl.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+  externalClasses: ['iconfont', 'icon-pinglun', 'icon-fenxiang'],
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
