684896bc0fd71620343b53925197a231b2ebd263
DM <276061317@qq.com>
2021-04-28 20:16:59 +0800

    w

--- music-imooc/miniprogram/components/blog-ctrl/blog-ctrl.js
+++ music-imooc/miniprogram/components/blog-ctrl/blog-ctrl.js
@@ -1,23 +1,51 @@
 // components/blog-ctrl/blog-ctrl.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {},
+	externalClasses: ['iconfont', 'icon-pinglun', 'icon-fenxiang'],
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+		// 登录组件是否显示
+		loginShow: false,
+		// 底部弹出层是否显示
+		modalShow: false,
+		content: '',
+	},
 
-  },
-  externalClasses: ['iconfont', 'icon-pinglun', 'icon-fenxiang'],
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+		onComment() {
+			this.setData({
+				loginShow: true,
+			})
+		},
+		onLoginsuccess() {
+			this.setData({
+				loginShow: false,
+			})
+		},
+		onLoginfail() {
+			wx.showModal({
+				title: '动动小指',
+				content: '请点授权哟~亲',
+				showCancel: true,
+				cancelText: '取消',
+				cancelColor: '#000000',
+				confirmText: '确定',
+				confirmColor: '#3CC51F',
+				success: (result) => {
+					if (result.confirm) {
+						this.onComment()
+					}
+				},
+			})
+		},
+		subscribeMsg() {},
+	},
 })
